c91680fcde4ce9526afd9bffe42803c3269985ea
Sushant <Sushant>
2017-06-06 10:57:43 +0530

    added code

---
+++ app/footer/footer.js
@@ -0,0 +1,11 @@
+(function(){
+	angular.module("footer",[])
+	
+	angular.module("footer")
+	.config(function(){
+		console.log("i am footer module config method..");
+	})
+	.run(function(){
+		console.log("i am footer module run method..");
+	})
+})();
---
+++ app/footer/footerCtrl.js
@@ -0,0 +1,8 @@
+(function(){
+	function footerCtrlFn(){
+		var vm=this;
+		
+	}
+	angular.module("footer")
+	.controller("footerCtrl",[footerCtrlFn]);
+})();
